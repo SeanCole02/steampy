2e72b4bb254fdae73d469c5e294502676b887237
vytal <80233205+SeanCole02@users.noreply.github.com>
2024-04-21 17:11:53 +0100

    Update client.py

--- steampy/client.py
+++ steampy/client.py
@@ -392,6 +392,7 @@ class SteamClient:
         }
 
         response = self._session.post(url, data=params, headers=headers).json()
+        print(response, response.status)
         if response.get('needs_mobile_confirmation'):
             response.update(self._confirm_transaction(response['tradeofferid']))
 
